5e7194ed21da6e4850be673850214627e409cf39
aaronsabellek <77847547+aaronsabellek@users.noreply.github.com>
2021-01-25 09:34:35 +0100

    Update text to melody converter.py

--- text to melody converter.py
+++ text to melody converter.py
@@ -5,29 +5,29 @@
 
 
 import codecs
-from pyo import *
 from time import sleep
 
 
-#create reference frequencies (equal temperament)
-h = 246.942
-ais = b = 233.082
-a = 220
-gis = as_ = 207.652
-g = 195.998
-fis = ges = 184.997
-f = 174.614
-e = 164.814
-dis = es = 155.563
-d = 146.832
-cis = des = 138.591
-c = 130.813
+#cclass to connect characters to frequencies
+class Connect():
+    #create reference frequencies (equal temperament)
+    h = 246.942
+    ais = b = 233.082
+    a = 220
+    gis = as_ = 207.652
+    g = 195.998
+    fis = ges = 184.997
+    f = 174.614
+    e = 164.814
+    dis = es = 155.563
+    d = 146.832
+    cis = des = 138.591
+    c = 130.813
 
-
-#connect characters to frequencies
-#C = c/2, c1 = c*2, c2 = c*4..
-#connections for illustration in c minor
-connections = {
+    #connect characters to frequencies in dictionary
+    #C = c/2, c1 = c*2, c2 = c*4..
+    #connections for illustration in c minor
+    connections = {
     "e": c/2,   #17.40% (relative frequency in german language)
     "n": c,     #9.78%
     "i": c*2,   #7.55%
@@ -55,57 +55,40 @@ connections = {
     "y": b/2,   #0.04%
     "x": b,     #0.03%
     "q": b*2    #0.02%
-}
+    }
 
 
-#function for translating characters to frequencies
-def freq(char):
-    if char in connections:
-        return connections[char]
-    else:
-        return 0
-
-
-#read text file and save as string
-#text for illustration converted to lowercase only and without umlauts and breaks
-with codecs.open("text_example.txt", "r", "utf-8") as text_file:
-    text = text_file.read().lower().replace("\n", " ").replace("ö", "oe").replace("ä", "ae").replace("ü", "ue")
-
-
-#convert string into list of frequencies
-freq_list = []
-for char in text:
-    freq_list.append(freq(char))
-    del char
-
-
-#start pyo server
-s = Server().boot()
-s.start()
+#function to convert a text file into a list of frequencies
+def convert(text_name):
+    #create an object of Connect-class to connect characters to frequencies
+    connect = Connect()
     
+    #read text file and safe as string
+    #for illustration converted to lowercase and without umlauts or breaks
+    with codecs.open(text_name, "r", "utf-8") as text_file:
+        text = text_file.read().lower().replace("\n", " ").replace("ö", "oe").replace("ä", "ae").replace("ü", "ue")
 
-#create or upload sounds using pyo (http://ajaxsoundstudio.com/pyodoc/)
-#saw- and sine-waves for illustration
-bg_synth_1 = SuperSaw(freq=c/2, mul=0.1).out() #saw-wave for background (left)
-bg_synth_2 = SuperSaw(freq=c, mul=0.1).out(1) #saw-wave for background (right)
-
-main_synth = Sine(freq=0, mul=0.1) #sine-wave
-
-del_synth_1 = Delay(main_synth, delay=[0.1, 0.04], feedback=0.6).out() #delayed sine-wave for main sound (left)
-del_synth_2 = Delay(main_synth, delay=[0.1, 0.06], feedback=0.6).out(1) #delayed sine-wave for main sound (right)
-
-sleep(2) #play background synth for two seconds before melody starts
-
-
-#play melody
-#adjust sleep(x) to manipulate duration of single note in seconds
-for frequency in freq_list:
-    main_synth.freq = frequency
-    sleep(0.8)
-
-main_synth.freq = 0
-sleep(2) #play background synth for two seconds after melody already ended
-
+    #function to translate characters to frequencies
+    def freq(char):
+        if char in connect.connections:
+            return connect.connections[char]
+        else:
+            return 0
     
-#stop pyo server
-s.stop()
+    #convert characters to frequencies and safe in list
+    freq_list = []
+    for char in text:
+        freq_list.append(freq(char))
+        del char
+
+    #return list of frequencies
+    return freq_list
+
+
+#function to play the converted melody
+#adjust duration of single note in seconds
+def play(freq_list, synth, duration):
+    for frequency in freq_list:
+        synth.freq = frequency
+        sleep(duration)
+    synth.freq = 0
